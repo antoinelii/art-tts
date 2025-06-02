cb5aad03ee6e7cfe5b3af1cd93865b300f92362b
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-06-02 17:07:44 +0200

    [configs] params_v0 dim 25 n_heads 0

--- src/configs/params_v0.py
+++ src/configs/params_v0.py
@@ -6,13 +6,13 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # MIT License for more details.
 
-# from model.utils import fix_len_compatibility
+from model.utils import fix_len_compatibility
 
 # data parameters
-# wavs_dir = '../../data/LJSpeech_1.1/wavs'
-# artic_dir = '../../data/LJSpeech_1.1/encoded_audio_en'
-wavs_dir = "../LJ_samples"
-artic_dir = "../LJ_samples/encoded_audio_en"
+wavs_dir = "../../data/LJSpeech_1.1/wavs"
+artic_dir = "../../data/LJSpeech_1.1/encoded_audio_en"
+# wavs_dir = "../LJ_samples"
+# artic_dir = "../LJ_samples/encoded_audio_en"
 train_filelist_path = "resources/filelists/ljspeech/train_v0.txt"
 valid_filelist_path = "resources/filelists/ljspeech/valid_v0.txt"
 test_filelist_path = "resources/filelists/ljspeech/test_v0.txt"
@@ -24,13 +24,14 @@ n_spks = 1  # 247 for Libri-TTS filelist and 1 for LJSpeech
 spk_emb_dim = 64
 
 # encoder parameters
+n_ipa_feats = 25
 n_enc_channels = 192
 filter_channels = 768
 filter_channels_dp = 256
 n_enc_layers = 6
 enc_kernel = 3
 enc_dropout = 0.1
-n_heads = 2
+n_heads = 1  # normally 2 but need to divide n_ipa_feats
 window_size = 4
 
 # decoder parameters
@@ -42,9 +43,9 @@ pe_scale = 1000  # 1 for `grad-tts-old.pt` checkpoint
 # training parameters
 log_dir = "logs/new_exp"
 test_size = 4
-n_epochs = 10000
+n_epochs = 1  # 10000 normally
 batch_size = 16
 learning_rate = 1e-4
 seed = 37
 save_every = 1
-# out_size = fix_len_compatibility(2*22050//256)
+out_size = fix_len_compatibility(2 * 22050 // 256)
