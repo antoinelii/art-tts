a3e21944461b4a1890c0c904baf3537863edded9
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-06-12 17:49:47 +0200

    [text] Removed useless if from merge_diphtongues implem

--- src/text/converters.py
+++ src/text/converters.py
@@ -169,17 +169,14 @@ def ipa_to_ternary(
             elif char_ipa in _signicative_punc_:
                 ternary_seq.append(punc_tok)
         else:  # normal phoneme character
-            if char_ipa in diphtongues_ipa:
+            if merge_diphtongues & (char_ipa in diphtongues_ipa):
                 # Handle diphtongues by merging the two IPA char into one
                 # vector representation
                 emb_0 = ft.word_array(traits_list, char_ipa[0])
                 emb_1 = ft.word_array(traits_list, char_ipa[1])
-                if merge_diphtongues:  # merge the two embeddings into one
-                    mask = emb_0 == emb_1
-                    emb_24 = np.zeros_like(emb_0)  # shape: (1, N_traits)
-                    emb_24[mask] = emb_0[mask]
-                else:
-                    emb_24 = np.concatenate((emb_0, emb_1), axis=0)
+                mask = emb_0 == emb_1
+                emb_24 = np.zeros_like(emb_0)  # shape: (1, N_traits)
+                emb_24[mask] = emb_0[mask]
             elif ft.validate_word(char_ipa):
                 emb_24 = ft.word_array(traits_list, char_ipa)  # shape: (1, N_traits)
             else:
