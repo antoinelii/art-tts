c02ddc81f7dd45e739e76d316725ec5584cdb662
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-06-13 14:44:30 +0200

    Init generate standadized phnms scripts

---
+++ src/generate_phnm3.py
@@ -0,0 +1,60 @@
+import importlib
+import argparse
+from tqdm import tqdm
+import logging
+from pathlib import Path
+import numpy as np
+
+dataset_params = {
+    "mngu0": ("phnm.mngu0", "get_mngu0_phnm3", ".lab"),
+    "mspka": ("phnm.mspka", "get_mspka_phnm3", ".lab"),
+    "pb2007": ("phnm.pb2007", "get_pb2007_phnm3", ".phone"),
+    "mocha": ("phnm.mocha", "get_mocha_phnm3", ".phnm"),
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--phnm_dir",
+    type=str,
+)
+parser.add_argument(
+    "--save_dir",
+    type=str,
+)
+parser.add_argument(
+    "--dataset_name",
+    type=str,
+    default="mngu0",  # or "mspka", "pb2007", "mocha"
+)
+
+# Setup logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    phnm_dir = Path(args.phnm_dir)
+    save_dir = Path(args.save_dir)
+    dataset_name = args.dataset_name
+
+    if not phnm_dir.exists():
+        raise FileNotFoundError(f"Phoneme directory {phnm_dir} does not exist.")
+
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    mod_name, func_name, ext = dataset_params[dataset_name]
+    mod = importlib.import_module(mod_name)
+    get_phnm3 = getattr(mod, func_name)
+
+    phnm_files = sorted([f for f in phnm_dir.glob(f"*{ext}")])
+    logger.info(f"Found {len(phnm_files)} audio files in {phnm_dir}")
+
+    for phnm_file in tqdm(phnm_files, desc=f"Processing {dataset_name} phoneme files"):
+        try:
+            ipa_phnm3 = get_phnm3(phnm_file)
+            # Save the phoneme data
+            save_path = save_dir / f"{phnm_file.stem}_phnm3.npy"
+            np.save(save_path, ipa_phnm3)
+            logger.info(f"Saved phoneme data to {save_path}")
+        except Exception as e:
+            logger.error(f"Error processing {phnm_file}: {e}")
---
+++ src/scripts/gen_phnm.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH --job-name=gen_phnms     # Job name
+#SBATCH --partition=cpu             # Take a node from the 'gpu' partition
+#SBATCH --export=ALL                  # Export your environment to the compute node
+
+#SBATCH --output=%x-%A_%a.out # fichier de sortie (%j = job ID)
+#SBATCH --error=%x-%A_%a.err # fichier d’erreur (%j = job ID)
+#SBATCH --nodes=1 # reserver 1 nœud
+#SBATCH --ntasks=1 # reserver 4 taches (ou processus)
+#SBATCH --ntasks-per-node=1 # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --cpus-per-task=1 # reserver 10 CPU par tache (et memoire associee)
+#SBATCH --time=1:00:00 # temps maximal d’allocation "(HH:MM:SS)"
+#SBATCH --hint=nomultithread # desactiver l’hyperthreading
+#SBATCH --array=0 # job array with 4 tasks (0 to 3)
+
+module purge
+
+source ../.venv/bin/activate
+
+#mocha
+#SPEAKERS=(faet0 ffes0 fsew0 maps0 mjjn0 msak0)
+#SPK=${SPEAKERS[$SLURM_ARRAY_TASK_ID]}
+#
+##mngu0
+#SPEAKERS=(s1)
+#SPK=${SPEAKERS[$SLURM_ARRAY_TASK_ID]}
+#
+##mspka
+SPEAKERS=(cnz lls olm)
+SPK=${SPEAKERS[$SLURM_ARRAY_TASK_ID]}
+
+#pb2007
+#SPEAKERS=(spk1)
+#SPK=${SPEAKERS[$SLURM_ARRAY_TASK_ID]}
+
+echo "Running job on $(hostname)"
+echo "python: $(which python)"
+echo "python-version $(python -V)"
+
+set -x # activer l’echo des commandes
+
+echo "computation start $(date)"
+# launch your computation
+
+#srun python -u ./generate_phnm3.py --phnm_dir ../../../../scratch2/ali/data/mocha_timit/src_data/$SPK \
+#                                        --save_dir ../../../../scratch2/ali/data/mocha_timit/arttts/$SPK/phnm3 \
+#                                        --dataset_name mocha \
+#
+#srun python -u ./generate_phnm3.py --phnm_dir ../../../../scratch1/data/raw_data/MNGU0/phone_labels \
+#                                        --save_dir ../../../../scratch2/ali/data/MNGU0/arttts/$SPK/phnm3 \
+#                                        --dataset_name mngu0
+#
+srun python -u ./generate_phnm3.py --phnm_dir ../../../../scratch2/ali/data/MSPKA_EMA_ita/src_data/${SPK}_1.0.0/lab_1.0.0 \
+                                        --save_dir ../../../../scratch2/ali/data/MSPKA_EMA_ita/arttts/$SPK/phnm3 \
+                                        --dataset_name mspka
+
+#srun python -u ./generate_phnm3.py --phnm_dir ../../../../scratch2/ali/data/pb2007/src_data/$SPK \
+#                                        --save_dir ../../../../scratch2/ali/data/pb2007/arttts/$SPK/phnm3 \
+#                                        --dataset_name pb2007
+
+echo "computation end : $(date)"
