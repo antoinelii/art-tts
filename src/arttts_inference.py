393a9f708aa75df51525b2c1ed1f9f8953e0dc33
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-06-19 14:42:52 +0200

    Init arttts_inference scripts (operational on ljspeech)

---
+++ src/arttts_inference.py
@@ -0,0 +1,139 @@
+import numpy as np
+import logging
+from tqdm import tqdm
+import importlib
+from pathlib import Path
+
+import torch
+
+from model import GradTTS
+from data_phnm import PhnmArticDataset, PhnmBatchCollate
+from utils import TqdmLoggingHandler
+from paths import CKPT_DIR
+
+import argparse
+
+mylogger = logging.getLogger(__name__)
+mylogger.setLevel(logging.INFO)
+
+handler = TqdmLoggingHandler()
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+mylogger.addHandler(handler)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--data_dir",  # ex: ../../data
+    type=str,
+)
+parser.add_argument(
+    "--save_dir",  # ex: ../../data/LJSpeech-1.1/arttts_pred/v1/grad_200
+    type=str,
+)
+parser.add_argument(
+    "--filelist_path",  # ex resources/filelists/ljspeech/valid_v1.txt
+    type=str,
+)
+parser.add_argument("--version", type=str, default="v1")
+parser.add_argument("--ckpt_name", type=str, default="grad_210.pt")
+parser.add_argument("--params_name", type=str, default="params_v1")
+parser.add_argument("--device", type=str, default="cuda")
+parser.add_argument(
+    "--batch_size", type=int, default=16, help="Batch size for inference, default is 16"
+)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    data_dir = Path(args.data_dir)
+    save_dir = Path(args.save_dir)
+    filelist_path = Path(args.filelist_path)
+    version = args.version
+    ckpt_name = args.ckpt_name
+    params_name = args.params_name
+    device = args.device
+    batch_size = args.batch_size
+
+    save_dir.mkdir(parents=True, exist_ok=True)
+    params = importlib.import_module(f"configs.{params_name}")
+
+    mylogger.info("Start model init...")
+
+    model = GradTTS(
+        params.n_ipa_feats,
+        params.n_spks,
+        None if params.n_spks == 1 else params.spk_emb_dim,  # spk_emb_dim
+        params.n_enc_channels,
+        params.filter_channels,
+        params.filter_channels_dp,
+        params.n_heads,
+        params.n_enc_layers,
+        params.enc_kernel,
+        params.enc_dropout,
+        params.window_size,
+        params.n_feats,
+        params.dec_dim,
+        params.beta_min,
+        params.beta_max,
+        params.pe_scale,
+    ).cuda()
+
+    ckpt_filepath = CKPT_DIR / version / ckpt_name
+    ckpt_state_dict = torch.load(ckpt_filepath, map_location=torch.device(device))
+
+    model.load_state_dict(ckpt_state_dict)
+
+    mylogger.info("Model loaded from %s", ckpt_filepath)
+
+    mylogger.info("Start dataset init...")
+
+    dataset = PhnmArticDataset(
+        filelist_path,
+        data_root_dir=data_dir,
+        load_coder=False,
+        merge_diphtongues=params.merge_diphtongues,
+    )
+
+    mylogger.info("Dataset loaded with %d samples", len(dataset))
+
+    reorder_feats = params.reorder_feats
+    filepaths_list = dataset.filepaths_list
+    collator = PhnmBatchCollate()
+
+    model.eval()
+    with torch.no_grad():
+        with tqdm(
+            range(0, len(filepaths_list), batch_size),
+            total=len(filepaths_list) // batch_size,
+            desc="Inference",
+            position=1,
+            dynamic_ncols=True,
+        ) as progress_bar:
+            for i in progress_bar:
+                batch_filepaths = filepaths_list[i : i + batch_size]
+                phnm3_filepaths = [fp[1] for fp in batch_filepaths]
+                phnm_embs = [
+                    {"x": dataset.get_phnm_emb(phnm3_fp)}
+                    for phnm3_fp in phnm3_filepaths
+                ]
+                batch = collator(phnm_embs)
+                x = batch["x"].to(torch.float32)
+                x_lengths = batch["x_lengths"]
+                y_enc, y_dec, attn = model(
+                    x, x_lengths, n_timesteps=50
+                )  # (B, 16, T) x 2 , (B,1,T0,T)
+                y_enc_14 = y_enc[:, reorder_feats, :].detach().cpu()
+                y_dec_14 = y_dec[:, reorder_feats, :].detach().cpu()
+                for j, (filepath, y_enc_j, y_dec_j) in enumerate(
+                    zip(batch_filepaths, y_enc_14, y_dec_14)
+                ):
+                    sample_id = filepath[0].split("/")[-1][:-4]
+                    save_path = save_dir / f"{sample_id}.npy"
+                    y_enc_dec_j = np.vstack(
+                        [y_enc_j.numpy(), y_dec_j.numpy()]
+                    )  # (2, 14, T)
+                    np.save(save_path, y_enc_dec_j)
+                    mylogger.info(f"Saved {save_path}")
+
+    mylogger.info("Inference completed. All files saved to %s", save_dir)
+    mylogger.info("Total samples processed: %d", len(filepaths_list))
---
+++ src/scripts/arttts_inference.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=arttts_infer     # Job name
+#SBATCH --partition=gpu_p2             # Take a node from the 'gpu' partition
+# #SBATCH --export=ALL                  # Export your environment to the compute node
+
+#SBATCH --output=%x-%j.out # fichier de sortie (%j = job ID)
+#SBATCH --error=%x-%j.err # fichier d’erreur (%j = job ID)
+# #SBATCH --constraint=v100-16g # demander des GPU a 16 Go de RAM
+#SBATCH --nodes=1 # reserver 1 nœud
+#SBATCH --ntasks=1 # reserver 4 taches (ou processus)
+#SBATCH --ntasks-per-node=1 # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:1 # reserver 4 GPU
+#SBATCH --cpus-per-task=3 # reserver 10 CPU par tache (et memoire associee)
+#SBATCH --time=2:00:00 # temps maximal d’allocation "(HH:MM:SS)"
+# #SBATCH --qos=qos_gpu-dev # QoS
+#SBATCH --hint=nomultithread # desactiver l’hyperthreading
+#SBATCH --account=rec@v100 # comptabilite V100
+
+
+module purge
+
+source ../.venv/bin/activate
+
+echo "visible nvidia gpus $(nvidia-smi)"
+echo "Running job on $(hostname)"
+echo "python: $(which python)"
+echo "python-version $(python -V)"
+echo "CUDA_DEVICE: $CUDA_VISIBLE_DEVICES"
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True #avoid fragmentation issues?
+
+#python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}')"
+#python -c "import torch; print(f'cuda device: {torch.cuda.current_device()}')"
+
+set -x # activer l’echo des commandes
+
+echo "computation start $(date)"
+# launch your computation
+
+srun python -u arttts_inference.py  --data_dir ../../data/ \
+                                    --save_dir ../../data/LJSpeech-1.1/arttts_pred/v1/grad_210 \
+                                    --filelist_path resources/filelists/ljspeech/valid_v1.txt \
+                                    --version v1 \
+                                    --ckpt_name grad_210.pt \
+                                    --params_name params_v1 \
+                                    --device cuda \
+                                    --batch_size 16 \
+
+echo "computation end : $(date)"
