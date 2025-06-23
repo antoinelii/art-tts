4aeb69b9df2545d42c6e9d222d36b619d2cc6d08
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-06-23 14:34:37 +0200

    Dataset conditional arttts_inference.sh

--- src/scripts/arttts_inference.sh
+++ src/scripts/arttts_inference.sh
@@ -3,18 +3,19 @@
 #SBATCH --partition=gpu_p2             # Take a node from the 'gpu' partition
 # #SBATCH --export=ALL                  # Export your environment to the compute node
 
-#SBATCH --output=%x-%j.out # fichier de sortie (%j = job ID)
-#SBATCH --error=%x-%j.err # fichier d’erreur (%j = job ID)
+#SBATCH --output=%x-%A_%a.out # fichier de sortie (%j = job ID)
+#SBATCH --error=%x-%A_%a.err # fichier d’erreur (%j = job ID)
 # #SBATCH --constraint=v100-16g # demander des GPU a 16 Go de RAM
 #SBATCH --nodes=1 # reserver 1 nœud
 #SBATCH --ntasks=1 # reserver 4 taches (ou processus)
 #SBATCH --ntasks-per-node=1 # nombre de tache MPI par noeud (= nombre de GPU par noeud)
 #SBATCH --gres=gpu:1 # reserver 4 GPU
 #SBATCH --cpus-per-task=3 # reserver 10 CPU par tache (et memoire associee)
-#SBATCH --time=2:00:00 # temps maximal d’allocation "(HH:MM:SS)"
+#SBATCH --time=1:00:00 # temps maximal d’allocation "(HH:MM:SS)"
 # #SBATCH --qos=qos_gpu-dev # QoS
 #SBATCH --hint=nomultithread # desactiver l’hyperthreading
 #SBATCH --account=rec@v100 # comptabilite V100
+#SBATCH --array=0-2 # job array with 4 tasks (0 to 3)
 
 
 module purge
@@ -36,13 +37,48 @@ set -x # activer l’echo des commandes
 echo "computation start $(date)"
 # launch your computation
 
-srun python -u arttts_inference.py  --data_dir ../../data/ \
-                                    --save_dir ../../data/LJSpeech-1.1/arttts_pred/v1/grad_565 \
+DATASET=MSPKA_EMA_ita # MNGU0, mocha_timit, MSPKA_EMA_ita, pb2007
+CKPT_NAME=grad_4750
+MAIN_DATA_DIR=/lustre/fsn1/projects/rech/rec/commun/data
+
+# Define speakers conditionally
+if [ "$DATASET" = "LJSpeech-1.1" ]; then
+    echo "Running inference for dataset: $DATASET"
+    srun python -u arttts_inference.py  --data_dir ${MAIN_DATA_DIR}/ \
+                                    --save_dir ${MAIN_DATA_DIR}/LJSpeech-1.1/arttts_pred/v1/${CKPT_NAME} \
                                     --filelist_path resources/filelists/ljspeech/valid_v1.txt \
                                     --version v1 \
-                                    --ckpt_name grad_565.pt \
+                                    --ckpt_name ${CKPT_NAME}.pt \
                                     --params_name params_v1 \
                                     --device cuda \
-                                    --batch_size 16 \
+                                    --batch_size 16
+else
+    if [ "$DATASET" = "MNGU0" ]; then
+        SPEAKERS=(s1)
+    elif [ "$DATASET" = "mocha_timit" ]; then
+        SPEAKERS=(faet0 ffes0 fsew0 maps0 mjjn0 msak0)
+    elif [ "$DATASET" = "MSPKA_EMA_ita" ]; then
+        SPEAKERS=(cnz lls olm)
+    elif [ "$DATASET" = "pb2007" ]; then
+        SPEAKERS=(spk1)
+    else
+        echo "Unknown dataset: $DATASET"
+        exit 1
+    fi
+    SPK=${SPEAKERS[$SLURM_ARRAY_TASK_ID]}
+
+    echo "Running inference for dataset: $DATASET, speaker: $SPK"
+
+    srun python -u ./arttts_inference.py  --data_dir ${MAIN_DATA_DIR} \
+                                        --save_dir ${MAIN_DATA_DIR}/$DATASET/arttts/$SPK/arttts_pred/v1/${CKPT_NAME} \
+                                        --filelist_path resources/filelists/$DATASET/${SPK}_v1.txt \
+                                        --version v1 \
+                                        --ckpt_name ${CKPT_NAME}.pt \
+                                        --params_name params_v1 \
+                                        --device cuda \
+                                        --batch_size 16
+fi
+
+
 
 echo "computation end : $(date)"
