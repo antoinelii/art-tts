f73f62f8f6e404a9f5673b92bc71ce4ea0b14a14
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-06-27 15:57:23 +0200

    renamed phnm module by utils_dataset, add read_mngu0_ema

--- src/utils_dataset/mngu0.py
+++ src/utils_dataset/mngu0.py
@@ -1,5 +1,6 @@
 import re
 import numpy as np
+import scipy
 
 # from mngu0 ipa conversion table mngu0_s1_symbol_table.pdf
 mngu02ipa = {
@@ -104,3 +105,73 @@ def get_mngu0_phnm3(lab_file: str) -> np.ndarray:
         start_time = end_time
     phones = np.array(phones, dtype=[("start", "f4"), ("end", "f4"), ("phone", "U10")])
     return phones
+
+
+MNGU0_features = [
+    "T3_py",
+    "T3_pz",
+    "T2_py",
+    "T2_pz",
+    "T1_py",
+    "T1_pz",
+    "jaw_py",
+    "jaw_pz",
+    "upperlip_py",
+    "upperlip_pz",
+    "lowerlip_py",
+    "lowerlip_pz",
+]
+
+
+def read_mngu0_ema(raw_ema_fp):
+    """Reads MNGU0 EMA data from a raw file and returns the relevant features.
+    Args:
+        raw_ema_fp (str or Path): Path to the raw EMA file.
+    Returns:
+        np.ndarray: A 2D numpy array containing the EMA data for the specified features.
+    """
+    columns = {}
+    columns["time"] = 0
+    columns["present"] = 1
+
+    with open(raw_ema_fp, "rb") as f:
+        dummy_line = f.readline()  # EST File Track
+        datatype = f.readline().split()
+        f.readline()  # Byte Order
+        nframes = int(f.readline().split()[1])
+        nchannels = int(f.readline().split()[1])
+        dummy_line, datatype, nframes, nchannels
+
+        while "CommentChar" not in str(f.readline(), "utf-8"):
+            pass
+        f.readline()  # empty line
+        line = f.readline()
+
+        while "EST_Header_End" not in str(line, "utf-8"):
+            line = str(line, "utf-8").strip("\n")
+            channel_number = int(line.split()[0].split("_")[1]) + 2
+            channel_name = line.split()[1]
+            columns[channel_name] = channel_number
+            line = f.readline()
+
+        ema_buffer = f.read()
+        data = np.frombuffer(ema_buffer, dtype="float32")
+        data_ = np.reshape(data, (-1, len(columns)))
+
+        feat_idx = [columns[feat] for feat in MNGU0_features]
+
+        ema_data = data_[:, feat_idx]
+        ema_data = ema_data * 100  # initial data in  10^-5m , we turn it to mm
+        nonan = 1
+        if np.isnan(ema_data).sum() != 0:
+            nonan = 0
+            # Build a cubic spline out of non-NaN values.
+            spline = scipy.interpolate.splrep(
+                np.argwhere(~np.isnan(ema_data).ravel()),
+                ema_data[~np.isnan(ema_data)],
+                k=3,
+            )
+            # Interpolate missing values and replace them.
+            for j in np.argwhere(np.isnan(ema_data)).ravel():
+                ema_data[j] = scipy.interpolate.splev(j, spline)
+    return ema_data, nonan
