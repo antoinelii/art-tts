de66dbc5ac8135839f1e04a6657a04dd26ad536f
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-07-10 17:32:14 +0200

    Add original _punctuation_ori list and symbols for Grad-TTS

--- src/text/symbols.py
+++ src/text/symbols.py
@@ -1,14 +1,15 @@
-""" from https://github.com/keithito/tacotron """
+"""from https://github.com/keithito/tacotron"""
 
 from text import cmudict
 
-_pad        = '_'
-_punctuation = '!\'(),.:;? "|'   # Added '"' and "|"
-_special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_pad = "_"
+_punctuation = "!'(),.:;? \"|"  # Added '"' and "|"
+_punctuation_ori = "!'(),.:;? "
+_special = "-"
+_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness:
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
+_arpabet = ["@" + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [_pad] + list(_special) + list(_punctuation_ori) + list(_letters) + _arpabet
