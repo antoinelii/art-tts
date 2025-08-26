46d8c372617bd410a23438eff696eda51b00b006
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-08-26 18:02:34 +0200

    Add arttts_inference_ms.py

---
+++ src/arttts_inference_ms.py
@@ -0,0 +1,218 @@
+import numpy as np
+import logging
+from tqdm import tqdm
+import importlib
+from pathlib import Path
+
+import torch
+
+from model_ms import GradTTArtic
+from data_ms import PhnmDataset, PhnmBatchCollate
+
+from voxcommunis.decoder import FeatureDecoder
+from voxcommunis.data import FeatureTokenizer
+
+from utils import TqdmLoggingHandler
+from paths import CKPT_DIR
+
+import argparse
+
+mylogger = logging.getLogger(__name__)
+mylogger.setLevel(logging.INFO)
+
+handler = TqdmLoggingHandler()
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+mylogger.addHandler(handler)
+
+
+def init_model(params):
+    model = GradTTArtic(
+        n_ipa_feats=params.n_ipa_feats,  # 26, 24 phonological traits + 1 silence dim + 1 phoneme repetition count
+        spk_emb_dim=params.spk_emb_dim,  # 64
+        n_enc_channels=params.n_enc_channels,  # 192
+        filter_channels=params.filter_channels,  # 768
+        filter_channels_dp=params.filter_channels_dp,  # 256
+        n_heads=params.n_heads,  # 2
+        n_enc_layers=params.n_enc_layers,  # 6
+        enc_kernel=params.enc_kernel,  # 3
+        enc_dropout=params.enc_dropout,  # 0.1
+        window_size=params.window_size,  # 4
+        n_feats=params.n_feats,  # 16 (articulatory features)
+        dec_dim=params.dec_dim,  # 64
+        beta_min=params.beta_min,  # 0.05
+        beta_max=params.beta_max,  # 20.0
+        pe_scale=params.pe_scale,  # 1000
+        spk_preemb_dim=1024,  # Similar
+    ).to(device)
+    return model
+
+
+def get_dataset(dataset_dir, manifest_path, alignment_path, separate_files, tokenizer):
+    dataset = PhnmDataset(
+        dataset_dir=dataset_dir,
+        separate_files=separate_files,
+        manifest_path=manifest_path,
+        alignment_path=alignment_path,
+        feature_tokenizer=tokenizer,
+        reorder_feats=params.reorder_feats,
+        pitch_idx=params.pitch_idx,
+        loudness_idx=params.loudness_idx,
+        log_normalize_loudness=params.log_normalize_loudness,
+        random_seed=params.random_seed,
+    )
+    return dataset
+
+
+def get_inputs(batch_file_ids, dataset, collator):
+    batch = []
+    for file_id in batch_file_ids:
+        phon_features = dataset.get_phon_feats(file_id)  # (n_ipa_feats, seq_len)
+        spk_preemb = dataset.get_spk_features(file_id)  # (1024,)
+        batch.append(
+            {
+                "x": phon_features,
+                "spk_ft": spk_preemb,
+            }
+        )
+    batch = collator(batch)  # in place
+    x, x_lengths, spk_ft = batch["x"], batch["x_lengths"], batch["spk_ft"]
+    return x, x_lengths, spk_ft
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--dataset_dir",
+    type=str,
+)
+parser.add_argument(
+    "--save_dir",
+    type=str,
+)
+parser.add_argument(
+    "--manifest_path",
+    type=str,
+)
+parser.add_argument(
+    "--alignment_path",
+    type=str,
+)
+parser.add_argument("--version", type=str, default="v6")
+parser.add_argument("--ckpt_name", type=str, default="grad_1000.pt")
+parser.add_argument("--params_name", type=str, default="params_v6")
+parser.add_argument("--device", type=str, default="cuda")
+parser.add_argument(
+    "--batch_size",
+    type=int,
+    default=1,
+    help="Batch size for inference, default is 1 and should be done with 1\
+        sample at a time to avoid messing up with group normalization",  # should remove groupnorm in future versions
+)
+
+parser.add_argument(
+    "--max_samples",
+    type=int,
+    default=0,
+    help="Maximum number of samples to process. 0 means all samples (default: 0)",
+)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    dataset_dir = Path(args.dataset_dir)
+    save_dir = Path(args.save_dir)
+    manifest_path = Path(args.manifest_path)
+    alignment_path = Path(args.alignment_path)
+    if manifest_path.is_file():
+        separate_files = False
+    else:  # directory with multiple manifests
+        separate_files = True
+    version = args.version
+    ckpt_name = args.ckpt_name
+    params_name = args.params_name
+    device = args.device
+    batch_size = args.batch_size
+
+    save_dir.mkdir(parents=True, exist_ok=True)
+    params = importlib.import_module(f"configs.{params_name}")
+
+    mylogger.info("Start model init...")
+
+    model = init_model(params, device)
+
+    ckpt_filepath = CKPT_DIR / version / ckpt_name
+    ckpt_state_dict = torch.load(ckpt_filepath, map_location=torch.device(device))
+
+    model.load_state_dict(ckpt_state_dict, weight_only=True)
+
+    mylogger.info("Model loaded from %s", ckpt_filepath)
+
+    mylogger.info("Getting dataset ...")
+
+    fd = FeatureDecoder(sum_diphthong=True)
+    tokenizer = FeatureTokenizer(fd)
+    dataset = get_dataset(
+        dataset_dir,
+        manifest_path,
+        alignment_path,
+        separate_files=separate_files,
+        tokenizer=tokenizer,
+    )
+
+    mylogger.info("Dataset loaded with %d samples", len(dataset))
+
+    reorder_feats = params.reorder_feats
+
+    file_ids = [e[0] for e in dataset.manifest]
+    if args.max_samples > 0:
+        file_ids = file_ids[: args.max_samples]
+        mylogger.info("Process only the first %d samples", args.max_samples)
+
+    collator = PhnmBatchCollate()
+
+    model.eval()
+    with torch.no_grad():
+        with tqdm(
+            range(0, len(file_ids), batch_size),
+            total=len(file_ids) // batch_size,
+            desc="Inference",
+            position=1,
+            dynamic_ncols=True,
+        ) as progress_bar:
+            for i in progress_bar:
+                # Prepare batch
+                batch_file_ids = file_ids[i : i + batch_size]
+                # Run inference
+                x, x_lengths, spk_ft = get_inputs(batch_file_ids, dataset, collator)
+                y_enc, y_dec, attn = model(
+                    x, x_lengths, spk_ft, n_timesteps=50
+                )  # (B, 16, T) x 2 , (B,1,T0,T)
+
+                y_enc_ = y_enc[:, reorder_feats, :].detach().cpu()  # (B, 14, T)
+                y_dec_ = y_dec[:, reorder_feats, :].detach().cpu()  # (B, 14, T)
+
+                for j, (file_id, y_enc_j, y_dec_j) in enumerate(
+                    zip(batch_file_ids, y_enc_, y_dec_)
+                ):
+                    x_len = x_lengths[j]
+                    attn_j = attn[j, 0, :x_len, :].detach().cpu()  # (x_len, y_len_max)
+                    input_map = np.where(
+                        attn_j == 1
+                    )[
+                        0
+                    ]  # (y_len,)  easier storage with enc & dec gives the index of the matching input
+                    y_len = np.max(
+                        np.where(attn_j[-1])
+                    )  # last row, last value=1 index (binary attn)
+                    save_path = save_dir / f"{file_id}.npy"
+                    y_enc_dec_j = np.vstack(
+                        [
+                            y_enc_j[:, : y_len + 1].numpy(),
+                            y_dec_j[:, : y_len + 1].numpy(),
+                            input_map,
+                        ]
+                    )  # (29, T)  # 29 (14 enc + 14 dec + 1 input_map)
+                    np.save(save_path, y_enc_dec_j)
+                    mylogger.info(f"Saved {save_path}")
+
+    mylogger.info("Inference completed. All files saved to %s", save_dir)
+    mylogger.info("Total samples processed: %d", len(file_ids))
--- src/data_ms.py
+++ src/data_ms.py
@@ -233,3 +233,156 @@ class PhnmArticBatchCollate(object):
             "y_lengths": y_lengths,
             "spk_ft": spk_ft,
         }
+
+
+class PhnmDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        dataset_dir: MyPathLike,  # where the split dir is located ()"train-1h" for instance)
+        manifest_path: MyPathLike,
+        alignment_path: MyPathLike,
+        feature_tokenizer: FeatureTokenizer,
+        separate_files: bool = False,
+        reorder_feats=reorder_feats,
+        pitch_idx=pitch_idx,
+        loudness_idx=loudness_idx,
+        log_normalize_loudness=log_normalize_loudness,
+        random_seed=random_seed,
+    ):
+        super().__init__()
+        self.feature_tokenizer = feature_tokenizer
+        self.init_manifest_and_alignments(
+            manifest_path, alignment_path, separate_files=separate_files
+        )
+        # articulatory settings
+        self.dataset_dir = dataset_dir
+        self.reorder_feats = reorder_feats
+        self.pitch_idx = pitch_idx
+        self.loudness_idx = loudness_idx
+        self.log_normalize_loudness = log_normalize_loudness
+
+        random.seed(random_seed)
+
+    def init_manifest_and_alignments(
+        self,
+        manifest_path: MyPathLike,
+        alignment_path: MyPathLike,
+        separate_files: bool = False,
+    ):
+        panphon_inventory = PanPhonInventory()
+        if separate_files:
+            manifests_list = sorted(list(Path(manifest_path).glob("*.tsv")))
+            self.langs = [fp.stem for fp in manifests_list]
+            self.lang_sizes = []
+            self.manifest = []
+            for man_path in manifests_list:
+                man = read_manifest(man_path)
+                self.manifest += list(man.items())
+                self.lang_sizes.append(len(man))
+
+            self.ipa_phones: dict[str, str] = {}
+            for lang in self.langs:
+                align_path = Path(alignment_path) / f"{lang}.align"
+                alignments = read_alignment(align_path)
+                self.ipa_phones.update(
+                    {
+                        file: panphon_inventory.convert_to_ipa(_align)
+                        for file, _align in alignments.items()
+                    }
+                )
+        else:
+            manifest = read_manifest(manifest_path)
+            self.manifest = list(manifest.items())
+            alignments = read_alignment(alignment_path)
+            assert self.feature_tokenizer.multilingual_mode  # ???
+            self.ipa_phones = {
+                file: panphon_inventory.convert_to_ipa(_align)
+                for file, _align in alignments.items()
+            }
+
+    def get_phon_feats(self, file_id: str) -> tuple[torch.Tensor]:
+        phones = self.ipa_phones[file_id].split(" ")  # parsed as list of str
+        phones, counts = unique_consecutive(phones, return_counts=True)
+        counts1 = [1 for _ in phones]  # create counts of 1 for each phone
+        phon_features, phones = self.feature_tokenizer.encode(phones, counts1)
+        # Add silence trait as an additional 25th dimension
+        sil_trait = (phon_features == 0).all(
+            axis=1
+        ) * 2 - 1  # 1 for sil, -1 for non-sil
+        phon_features = torch.concat([phon_features, sil_trait.unsqueeze(1)], dim=1)
+        # add counts to the as an additional 26th dimension
+        counts = torch.as_tensor(counts, dtype=torch.float).unsqueeze(1)
+        phon_features = torch.concat([phon_features, counts], dim=1)
+        phon_features = torch.FloatTensor(phon_features).T
+        return phon_features  # shape: (n_ipa_feats, seq_len)
+
+    def get_spk_features(
+        self,
+        file_id: str,
+    ) -> torch.FloatTensor:  # shape: (n_art_feats, T)
+        """
+        Get spk prembedding features
+        Args:
+            file_id (str): sample uuid.
+        Returns:
+            torch.FloatTensor: spk preemb features (shape: (1024,)).
+        """
+
+        lang = file_id.split("_")[2]
+        encoded_dir = Path(self.dataset_dir) / "encoded_audio_multi" / lang
+        spk_preemb_fp = encoded_dir / "spk_preemb" / f"{file_id}.npy"
+
+        if spk_preemb_fp.exists():
+            spk_preemb = np.load(spk_preemb_fp)
+        else:
+            raise FileNotFoundError(
+                f"Preprocessed file {spk_preemb_fp} does not exist."
+            )
+
+        return torch.FloatTensor(spk_preemb)  # shape: (1024,)
+
+    def __getitem__(self, index):
+        if index >= len(self.manifest):
+            raise IndexError(f"Index {index} out of range")
+
+        file_id, (audio_path, num_samples) = self.manifest[index]
+        phon_features = self.get_phon_feats(file_id)  # (n_ipa_feats, seq_len)
+        spk_preemb = self.get_spk_features(file_id)  # (1024,)
+        item = {
+            "x": phon_features,
+            "spk_ft": spk_preemb,
+        }
+        return item
+
+    def __len__(self):
+        return len(self.manifest)
+
+    def sample_test_batch(self, size):
+        idx = np.random.choice(range(len(self)), size=size, replace=False)
+        test_batch = []
+        for index in idx:
+            test_batch.append(self.__getitem__(index))
+        return test_batch
+
+
+class PhnmBatchCollate(object):
+    def __call__(self, batch):
+        B = len(batch)
+        x_max_length = max([item["x"].shape[-1] for item in batch])
+        n_ipa_feats = batch[0]["x"].shape[-2]
+
+        spk_ft = torch.stack([item["spk_ft"] for item in batch], dim=0)
+        x = torch.zeros((B, n_ipa_feats, x_max_length), dtype=torch.float32)
+        x_lengths = []
+
+        for i, item in enumerate(batch):
+            x_ = item["x"]
+            x_lengths.append(x_.shape[-1])
+            x[i, :, : x_.shape[-1]] = x_
+
+        x_lengths = torch.LongTensor(x_lengths)
+        return {
+            "x": x,
+            "x_lengths": x_lengths,
+            "spk_ft": spk_ft,
+        }
