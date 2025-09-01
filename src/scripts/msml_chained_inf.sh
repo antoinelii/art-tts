d908b574da275fec41f917e5d2b98043d867ff2d
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-09-01 14:38:16 +0200

    Corrected chained scripts path adding 'scripts/'

--- src/scripts/msml_chained_inf.sh
+++ src/scripts/msml_chained_inf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-JID_JOB1=`sbatch msml_arttts_inf.sh | cut -d " " -f 4`
-JID_JOB2=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes msml_quanti_comp.sh | cut -d " " -f 4`
-JID_JOB3=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes msml_hifigan_inf.sh | cut -d " " -f 4`
+JID_JOB1=`sbatch scripts/msml_arttts_inf.sh | cut -d " " -f 4`
+JID_JOB2=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes scripts/msml_quanti_comp.sh | cut -d " " -f 4`
+JID_JOB3=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes scripts/msml_hifigan_inf.sh | cut -d " " -f 4`
