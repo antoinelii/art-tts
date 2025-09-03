e9690c5b7c138a4f8043cc33c597ce3594893809
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-09-03 15:16:10 +0200

    Run .sh inference for msml1h 2000 on dev-1h for train langs

--- src/scripts/msml_quanti_comp.sh
+++ src/scripts/msml_quanti_comp.sh
@@ -36,7 +36,7 @@ echo "computation start $(date)"
 DATASET=VoxCommunis
 MAIN_DATA_DIR=/lustre/fsn1/projects/rech/rec/commun/data
 SPLIT=dev-1h
-for E in 1000; do
+for E in 2000; do
     CKPT_NAME=grad_${E}
     for MODEL_VERSION in msml1h; do
         #LANGs that were present in the training data
