4c1a65b4c8a39680249243b5d07246e08ea439d5
antoinelii <91544027+antoinelii@users.noreply.github.com>
2025-09-04 14:12:33 +0200

    Use ms_quanti_gt_comp_custom instead of ms_quanti_comp_custom in ms_chained_inf_custom

--- src/scripts/ms_chained_inf_custom.sh
+++ src/scripts/ms_chained_inf_custom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 JID_JOB1=`sbatch scripts/ms_arttts_inf_custom.sh | cut -d " " -f 4`
-JID_JOB2=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes scripts/ms_quanti_comp_custom.sh | cut -d " " -f 4`
+JID_JOB2=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes scripts/ms_quanti_gt_comp_custom.sh | cut -d " " -f 4`
 JID_JOB3=`sbatch --dependency=afterok:$JID_JOB1 --kill-on-invalid-dep=yes scripts/ms_hifigan_inf_custom.sh | cut -d " " -f 4`
