30b7ae9b769bcacfa767258902c569fe7e4e4500
Antoine LI <91544027+antoinelii@users.noreply.github.com>
2026-01-28 03:08:28 +0100

    [clean] Move bash scripts to dedicated folder

---
+++ scripts/bash_scripts/ms_hifigan_inf.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#SBATCH --job-name=ms_hifigan_inf     # Job name
+#SBATCH --partition=gpu_p2             # Take a node from the 'gpu' partition
+# #SBATCH --export=ALL                  # Export your environment to the compute node
+
+#SBATCH --output=%x-%A_%a.out # fichier de sortie (%j = job ID)
+#SBATCH --error=%x-%A_%a.err # fichier d’erreur (%j = job ID)
+# #SBATCH --constraint=v100-16g # demander des GPU a 16 Go de RAM
+#SBATCH --nodes=1 # reserver 1 nœud
+#SBATCH --ntasks=1 # reserver 4 taches (ou processus)
+#SBATCH --ntasks-per-node=1 # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:1 # reserver 4 GPU
+#SBATCH --cpus-per-task=3 # reserver 10 CPU par tache (et memoire associee)
+#SBATCH --time=4:00:00 # temps maximal d’allocation "(HH:MM:SS)"
+# #SBATCH --qos=qos_gpu-dev # QoS
+#SBATCH --hint=nomultithread # desactiver l’hyperthreading
+#SBATCH --account=rec@v100 # comptabilite V100
+#SBATCH --array=0 # job array with 4 tasks (0 to 3)
+
+
+module purge
+
+source ../.venv/bin/activate
+
+echo "visible nvidia gpus $(nvidia-smi)"
+echo "Running job on $(hostname)"
+echo "python: $(which python)"
+echo "python-version $(python -V)"
+echo "CUDA_DEVICE: $CUDA_VISIBLE_DEVICES"
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True #avoid fragmentation issues?
+
+#python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}')"
+#python -c "import torch; print(f'cuda device: {torch.cuda.current_device()}')"
+
+set -x # activer l’echo des commandes
+
+echo "computation start $(date)"
+# launch your computation
+
+DATASET=VoxCommunis
+MAIN_DATA_DIR=/lustre/fsn1/projects/rech/rec/commun/data
+SPLIT=test-20h
+SPLIT=dev-1h
+
+VOCODER=sparc_en
+
+for E in 500 1000 2000 3000 4000 5000; do
+    CKPT_NAME=grad_${E}
+    for MODEL_VERSION in v6 v6_zhCN; do
+        for LANG in fr; do
+            echo "Running inference for dataset: $DATASET, src_art : $MODEL_VERSION $CKPT_NAME decoder"
+            srun python -u ./scripts/inference/hifigan_inference_ms.py  --data_dir ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/arttts_pred/${MODEL_VERSION}/${CKPT_NAME} \
+                                                --save_dir ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/hifigan_pred/${MODEL_VERSION}/${CKPT_NAME}/${VOCODER}/ \
+                                                --sparc_dir ${MAIN_DATA_DIR}/${DATASET}/encoded_audio_multi/${LANG} \
+                                                --manifest_path ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/manifests/${LANG}.tsv \
+                                                --version ${MODEL_VERSION} \
+                                                --generator_ckpt ckpt/${VOCODER}.ckpt \
+                                                --params_name params_${MODEL_VERSION} \
+                                                --device cuda \
+                                                --src_art decoder
+            
+            #echo "Running inference for dataset: $DATASET, src_art : sparc"
+            #srun python -u ./scripts/inference/hifigan_inference_ms.py  --data_dir ${MAIN_DATA_DIR}/${DATASET}/encoded_audio_multi/${LANG}/emasrc \
+            #                                    --save_dir ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/hifigan_pred/sparc/${VOCODER}/ \
+            #                                    --sparc_dir ${MAIN_DATA_DIR}/${DATASET}/encoded_audio_multi/${LANG} \
+            #                                    --manifest_path ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/manifests/${LANG}.tsv \
+            #                                    --version ${MODEL_VERSION} \
+            #                                    --generator_ckpt ckpt/${VOCODER}.ckpt \
+            #                                    --params_name params_${MODEL_VERSION} \
+            #                                    --device cuda        
+        done
+    done
+done
+
+##do sparc generation only once
+#LANG=fr
+#echo "Running inference for dataset: $DATASET, src_art : sparc"
+#srun python -u ./scripts/inference/hifigan_inference_ms.py  --data_dir ${MAIN_DATA_DIR}/${DATASET}/encoded_audio_multi/${LANG}/emasrc \
+#                                    --save_dir ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/hifigan_pred/sparc/${VOCODER}/ \
+#                                    --sparc_dir ${MAIN_DATA_DIR}/${DATASET}/encoded_audio_multi/${LANG} \
+#                                    --manifest_path ${MAIN_DATA_DIR}/${DATASET}/${SPLIT}/manifests/${LANG}.tsv \
+#                                    --version ${MODEL_VERSION} \
+#                                    --generator_ckpt ckpt/${VOCODER}.ckpt \
+#                                    --params_name params_${MODEL_VERSION} \
+#                                    --device cuda
+#
+echo "computation end : $(date)"
